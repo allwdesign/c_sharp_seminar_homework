adc3abff7901015e84fd3e6cd360453e3dc45d8e
Natalia Kuzmenko <allwdesign@gmail.com>
2022-08-19 15:50:14 +0300

    Add solution for the task2

---
+++ first_homework/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the first integer number ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second integer number ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberA == numberB)
+{
+    Console.WriteLine("Both numbers are equal");
+}
+else if (numberA > numberB)
+{
+    Console.WriteLine("max = " + numberA + ", min = " + numberB);
+}
+else
+{
+    Console.WriteLine("max = " + numberB + ", min = " + numberA);
+}
