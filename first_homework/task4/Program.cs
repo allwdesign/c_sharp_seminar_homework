57f3ef7b4a046e54ded642ae1090d4179cceeda3
Natalia Kuzmenko <allwdesign@gmail.com>
2022-08-23 17:52:39 +0300

    Add solution for the task4

---
+++ first_homework/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter the first integer number ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second integer number ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the third integer number ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+
+if (numberA > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.Write("max = " + max);
