db2d284f3c40a83008c8fe6f4f6e96e1027c6128
Natalia Kuzmenko <allwdesign@gmail.com>
2022-08-23 19:03:31 +0300

    Add solution for task6

---
+++ first_homework/task6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the integer number ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+string msg = "Is " + numberA + " an even number?";
+
+Console.WriteLine(msg);
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Yes. This is an even number.");
+}
+else
+{
+   Console.WriteLine("No."); 
+}
+
