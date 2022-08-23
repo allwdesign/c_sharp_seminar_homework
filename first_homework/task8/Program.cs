c6d2df862062ae61d2955165be5fbb630ed70b40
Natalia Kuzmenko <allwdesign@gmail.com>
2022-08-23 19:31:31 +0300

    Add solution for the task8

---
+++ first_homework/task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter the integer number ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+string msg = "";
+
+for (int i=1; i<=numberN; i++)
+{
+    if (i % 2 == 0)
+    {
+        msg = msg + i + ", ";
+    }
+    
+}
+
+Console.WriteLine(msg);
