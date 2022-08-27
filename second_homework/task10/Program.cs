1b00834818b3619fbb9f820a23a6fe48ec408aef
Natalia Kuzmenko <allwdesign@gmail.com>
2022-08-27 18:46:44 +0300

    Add solution for task10

--- second_homework/task10/Program.cs
+++ second_homework/task10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+//n / 10 % 10 результат этого выражения
+//даст 2ю цифру трехзначного числа.
+Console.WriteLine(n / 10 % 10);
