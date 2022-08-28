386a6ab9d891b7a721966065dc895f8d6b9bdb4e
Natalia Kuzmenko <allwdesign@gmail.com>
2022-08-28 20:15:51 +0300

    Add solution for task*

--- second_homework/task/Program.cs
+++ second_homework/task/Program.cs
@@ -7,3 +7,88 @@
 //ваша программа правильно обработает все случаи, как минимум
 //до 1000 человек
 
+string CheckForNumber(int number)
+{   
+    //1 программист
+    //2 программиста
+    //5 программистов
+    //21 программист
+    //22 программиста
+    //100 программистов
+    //111 программистов
+    //101 программист
+    //150 программистов
+    //151 программист
+    //200 программистов
+    //201 программист
+    //999 программистов
+    
+    string end = string.Empty;
+        
+    //N-significant (N > 2) number of the type: 
+    //111, 211, 1111, 11111 ending in 11. 
+    if (number % 100 == 11) number = number % 100;
+    
+    //2-significant number 
+    if (number > 19) number = number % 10;
+
+    //1 digit - 0, от 5 до 19
+    if ((number <= 19 && number >= 5) || (number == 0)) end = "ов";
+    
+    //от 2 до 4 включительно
+    else if (number < 5 && number > 1) end = "а";
+    else
+    {
+        //number = 1
+        end = string.Empty;
+    }
+    return end;
+}
+
+string GetСorrectPlural(int number)
+{
+    string endString = string.Empty;
+    //от 0 до 19
+    endString = CheckForNumber(number);
+    return endString;
+}
+
+void PrintMessage(int number)
+{
+    string word = "программист";
+    //string msg = string.Empty;
+    string ending = GetСorrectPlural(number);
+
+    Console.WriteLine($"{number} {word}{ending}");
+}
+
+// void ProgramCheck()
+// {
+//     Console.WriteLine("-----------------PROGRAM CHECK-------------------");
+//     for (int i = 0; i <= 1000; i++)
+//     {
+//         PrintMessage(i); 
+//     }
+// }
+
+// ProgramCheck();
+
+Console.Write("Enter the number:  ");
+
+try
+{   
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 0)
+    {
+        throw new ArgumentException("You enter a number less than zero!");
+    }
+    PrintMessage(number);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch
+{
+    Console.WriteLine("You didn't enter a number!");
+}
