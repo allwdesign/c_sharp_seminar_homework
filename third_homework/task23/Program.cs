9d9f229e232987a25806829cf2454f0974e817d3
Natalia Kuzmenko <allwdesign@gmail.com>
2022-09-01 02:04:26 +0300

    Add solution for task23

---
+++ third_homework/task23/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+void PrintCubeOfNumberFrom1ToN(int n)
+{
+    int index = 1;
+    while (index <= n)
+    {
+        int cube = index * index * index;
+        if (index != n) Console.Write($"{cube}, ");
+        else Console.Write($"{cube}");
+        
+        index++;
+    }
+    Console.WriteLine();
+
+}
+
+try
+{
+    Console.Write("Enter N number ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    PrintCubeOfNumberFrom1ToN(num);
+}
+catch
+{
+    Console.WriteLine("You entered an invalid number!");
+}
